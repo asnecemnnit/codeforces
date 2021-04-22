c5a21ae9ead703237305acde316fe311e4ca8070
Ashish Singh Negi <asnecemnnit@gmail.com>
2021-04-22 05:54:57 +0200

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -98,6 +98,55 @@ vector<bool> SieveOfEratosthenes(int n=1000000)
 	return prime;
 }
 
+// isPrime[] : isPrime[i] is true if number is prime
+// prime[] : stores all prime number less than N
+// SPF[] that store smallest prime factor of number
+// [for Exp : smallest prime factor of '8' and '16'
+// is '2' so we put SPF[8] = 2 , SPF[16] = 2 ]
+const long long MAX_SIZE = 1000001;
+vector<long long >isprime(MAX_SIZE , true);
+vector<long long >prime;
+vector<long long >SPF(MAX_SIZE);
+/* function generate all prime number less then N in O(n) */
+void manipulated_seive(int N)
+{
+    // 0 and 1 are not prime
+    isprime[0] = isprime[1] = false ;
+
+    // Fill rest of the entries
+    for (long long int i=2; i<N ; i++)
+    {
+        // If isPrime[i] == True then i is
+        // prime number
+        if (isprime[i])
+        {
+            // put i into prime[] vector
+            prime.push_back(i);
+
+            // A prime number is its own smallest
+            // prime factor
+            SPF[i] = i;
+        }
+
+        // Remove all multiples of  i*prime[j] which are
+        // not prime by making isPrime[i*prime[j]] = false
+        // and put smallest prime factor of i*Prime[j] as prime[j]
+        // [ for exp :let  i = 5 , j = 0 , prime[j] = 2 [ i*prime[j] = 10 ]
+        // so smallest prime factor of '10' is '2' that is prime[j] ]
+        // this loop run only one time for number which are not prime
+        for (long long int j=0;
+             j < (int)prime.size() &&
+             i*prime[j] < N && prime[j] <= SPF[i];
+             j++)
+        {
+            isprime[i*prime[j]]=false;
+
+            // put smallest prime factor of i*prime[j]
+            SPF[i*prime[j]] = prime[j] ;
+        }
+    }
+}
+
 /* Returns length of longest common subsequence for X[0..m-1], Y[0..n-1] */
 int lcs(string X, string Y, int m, int n )
 {
@@ -166,4 +215,169 @@ int LCSubStr(string X, string Y, int m, int n)
         }
     }
     return result;
+}
+
+/*	At each iteration the vertex v is selected
+	which has the smallest distance d[v] among
+	all the unmarked vertices. If the distance
+	to selected vertex v is equal to infinity,
+	the algorithm stops. Otherwise the vertex
+	is marked, and all the edges going out from
+	this vertex are checked. If relaxation along
+	the edge is possible (i.e. distance d[to]can
+	be improved), the distance d[to] and predecessor
+	p[to] are updated	*/
+const int INF = 1000000000;
+vector<vector<pair<int, int>>> adj;
+
+void dijkstra(int s, vector<int> & d, vector<int> & p) {
+    int n = adj.size();
+    d.assign(n, INF);
+    p.assign(n, -1);
+    vector<bool> u(n, false);
+
+    d[s] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+        for (int j = 0; j < n; j++) {
+            if (!u[j] && (v == -1 || d[j] < d[v]))
+                v = j;
+        }
+
+        if (d[v] == INF)
+            break;
+
+        u[v] = true;
+        for (auto edge : adj[v]) {
+            int to = edge.first;
+            int len = edge.second;
+
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+            }
+        }
+    }
+}
+
+/*	p[] stores the predecessors of all vertices
+	(except starting vertex s). The path to any vertex t
+	can be restored in the following way	*/
+vector<int> restore_path(int s, int t, vector<int> const& p) {
+    vector<int> path;
+
+    for (int v = t; v != s; v = p[v])
+        path.push_back(v);
+    path.push_back(s);
+
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+
+/* 	find a spanning tree of this graph which connects
+ 	all vertices and has the least weight (i.e. the sum
+	of weights of edges is minimal). A spanning tree is
+	a set of edges such that any vertex can reach any
+	other by exactly one simple path. The spanning tree
+	with the least weight is called a minimum spanning tree.
+
+	The minimum spanning tree is built gradually by adding
+	edges one at a time. At first the spanning tree consists
+	only of a single vertex (chosen arbitrarily). Then the
+	minimum weight edge outgoing from this vertex is selected
+	and added to the spanning tree. After that the spanning
+	tree already consists of two vertices. Now select and
+	add the edge with the minimum weight that has one end
+	in an already selected vertex (i.e. a vertex that is
+	already part of the spanning tree), and the other end
+	in an unselected vertex. And so on, i.e. every time we
+	select and add the edge with minimal weight that connects
+	one selected vertex with one unselected vertex. The
+	process is repeated until the spanning tree contains
+	all vertices (or equivalently until we have n−1 edges)	*/
+
+// Dense graphs : O(n2) time and O(n) memory
+int n;
+vector<vector<int>> adj; // adjacency matrix of graph
+const int INF = 1000000000; // weight INF means there is no edge
+
+struct Edge {
+    int w = INF, to = -1;
+};
+
+void prim() {
+    int total_weight = 0;
+    vector<bool> selected(n, false);
+    vector<Edge> min_e(n);
+    min_e[0].w = 0;
+
+    for (int i=0; i<n; ++i) {
+        int v = -1;
+        for (int j = 0; j < n; ++j) {
+            if (!selected[j] && (v == -1 || min_e[j].w < min_e[v].w))
+                v = j;
+        }
+
+        if (min_e[v].w == INF) {
+            cout << "No MST!" << endl;
+            exit(0);
+        }
+
+        selected[v] = true;
+        total_weight += min_e[v].w;
+        if (min_e[v].to != -1)
+            cout << v << " " << min_e[v].to << endl;
+
+        for (int to = 0; to < n; ++to) {
+            if (adj[v][to] < min_e[to].w)
+                min_e[to] = {adj[v][to], v};
+        }
+    }
+
+    cout << total_weight << endl;
+}
+
+// Sparse graphs : O(mlogn) time
+const int INF = 1000000000;
+struct Edge {
+    int w = INF, to = -1;
+    bool operator<(Edge const& other) const {
+        return make_pair(w, to) < make_pair(other.w, other.to);
+    }
+};
+int n;
+vector<vector<Edge>> adj;
+
+void prim() {
+    int total_weight = 0;
+    vector<Edge> min_e(n);
+    min_e[0].w = 0;
+    set<Edge> q;
+    q.insert({0, 0});
+    vector<bool> selected(n, false);
+    for (int i = 0; i < n; ++i) {
+        if (q.empty()) {
+            cout << "No MST!" << endl;
+            exit(0);
+        }
+
+        int v = q.begin()->to;
+        selected[v] = true;
+        total_weight += q.begin()->w;
+        q.erase(q.begin());
+
+        if (min_e[v].to != -1)
+            cout << v << " " << min_e[v].to << endl;
+
+        for (Edge e : adj[v]) {
+            if (!selected[e.to] && e.w < min_e[e.to].w) {
+                q.erase({min_e[e.to].w, e.to});
+                min_e[e.to] = {e.w, v};
+                q.insert({e.w, e.to});
+            }
+        }
+    }
+
+    cout << total_weight << endl;
 }
