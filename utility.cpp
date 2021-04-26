9901968939f409eb8af868a377998e22f190606b
Ashish Singh Negi <asnecemnnit@gmail.com>
2021-04-26 07:08:31 +0200

    Added Kruskal Algorithm for MST
    - Added Codeforces Round #674 (Div. 3) submissions

--- utility.cpp
+++ utility.cpp
@@ -677,6 +677,288 @@ void prim() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/* Finding MST using Kruskal's algorithm
+
+	1. Sort all the edges in non-decreasing order of their weight.
+	2. Pick the smallest edge. Check if it forms a cycle with the
+	spanning tree formed so far. If cycle is not formed, include
+	this edge. Else, discard it.
+	3. Repeat step#2 until there are (V-1) edges in the spanning tree. */
+
+// a structure to represent a
+// weighted edge in graph
+class Edge {
+public:
+    int src, dest, weight;
+};
+
+// a structure to represent a connected,
+// undirected and weighted graph
+class Graph {
+public:
+
+    // V-> Number of vertices, E-> Number of edges
+    int V, E;
+
+    // graph is represented as an array of edges.
+    // Since the graph is undirected, the edge
+    // from src to dest is also edge from dest
+    // to src. Both are counted as 1 edge here.
+    Edge* edge;
+};
+
+// Creates a graph with V vertices and E edges
+Graph* createGraph(int V, int E)
+{
+    Graph* graph = new Graph;
+    graph->V = V;
+    graph->E = E;
+
+    graph->edge = new Edge[E];
+
+    return graph;
+}
+
+// A structure to represent a subset for union-find
+class subset {
+public:
+    int parent;
+    int rank;
+};
+
+// A utility function to find set of an element i
+// (uses path compression technique)
+int find(subset subsets[], int i)
+{
+    // find root and make root as parent of i
+    // (path compression)
+    if (subsets[i].parent != i)
+        subsets[i].parent
+            = find(subsets, subsets[i].parent);
+
+    return subsets[i].parent;
+}
+
+// A function that does union of two sets of x and y
+// (uses union by rank)
+void Union(subset subsets[], int x, int y)
+{
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    // Attach smaller rank tree under root of high
+    // rank tree (Union by Rank)
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+
+    // If ranks are same, then make one as root and
+    // increment its rank by one
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+// Compare two edges according to their weights.
+// Used in qsort() for sorting an array of edges
+int myComp(const void* a, const void* b)
+{
+    Edge* a1 = (Edge*)a;
+    Edge* b1 = (Edge*)b;
+    return a1->weight > b1->weight;
+}
+
+// The main function to construct MST using Kruskal's
+// algorithm
+// Greedy approach
+// Step #2 uses the Union-Find algorithm to detect cycles
+void KruskalMST(Graph* graph)
+{
+    int V = graph->V;
+    Edge result[V]; // Tnis will store the resultant MST
+    int e = 0; // An index variable, used for result[]
+    int i = 0; // An index variable, used for sorted edges
+
+    // Step 1: Sort all the edges in non-decreasing
+    // order of their weight. If we are not allowed to
+    // change the given graph, we can create a copy of
+    // array of edges
+    qsort(graph->edge, graph->E, sizeof(graph->edge[0]),
+          myComp);
+
+    // Allocate memory for creating V ssubsets
+    subset* subsets = new subset[(V * sizeof(subset))];
+
+    // Create V subsets with single elements
+    for (int v = 0; v < V; ++v)
+    {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    // Number of edges to be taken is equal to V-1
+    while (e < V - 1 && i < graph->E)
+    {
+        // Step 2: Pick the smallest edge. And increment
+        // the index for next iteration
+        Edge next_edge = graph->edge[i++];
+
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+
+        // If including this edge does't cause cycle,
+        // include it in result and increment the index
+        // of result for next edge
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+        // Else discard the next_edge
+    }
+
+    // print the contents of result[] to display the
+    // built MST
+    cout << "Following are the edges in the constructed "
+            "MST\n";
+    int minimumCost = 0;
+    for (i = 0; i < e; ++i)
+    {
+        cout << result[i].src << " -- " << result[i].dest
+             << " == " << result[i].weight << endl;
+        minimumCost = minimumCost + result[i].weight;
+    }
+    // return;
+    cout << "Minimum Cost Spanning Tree: " << minimumCost
+         << endl;
+}
+
+// Kruskal Algorithm (STL approach)
+// Step #2 uses the Union-Find algorithm to detect cycles
+// Creating shortcut for an integer pair
+typedef  pair<int, int> iPair;
+
+// Structure to represent a graph
+struct Graph
+{
+    int V, E;
+    vector< pair<int, iPair> > edges;
+
+    // Constructor
+    Graph(int V, int E)
+    {
+        this->V = V;
+        this->E = E;
+    }
+
+    // Utility function to add an edge
+    void addEdge(int u, int v, int w)
+    {
+        edges.push_back({w, {u, v}});
+    }
+
+    // Function to find MST using Kruskal's
+    // MST algorithm
+    int kruskalMST();
+};
+
+// To represent Disjoint Sets
+struct DisjointSets
+{
+    int *parent, *rnk;
+    int n;
+
+    // Constructor.
+    DisjointSets(int n)
+    {
+        // Allocate memory
+        this->n = n;
+        parent = new int[n+1];
+        rnk = new int[n+1];
+
+        // Initially, all vertices are in
+        // different sets and have rank 0.
+        for (int i = 0; i <= n; i++)
+        {
+            rnk[i] = 0;
+
+            //every element is parent of itself
+            parent[i] = i;
+        }
+    }
+
+    // Find the parent of a node 'u'
+    // Path Compression
+    int find(int u)
+    {
+        /* Make the parent of the nodes in the path
+           from u--> parent[u] point to parent[u] */
+        if (u != parent[u])
+            parent[u] = find(parent[u]);
+        return parent[u];
+    }
+
+    // Union by rank
+    void merge(int x, int y)
+    {
+        x = find(x), y = find(y);
+
+        /* Make tree with smaller height
+           a subtree of the other tree  */
+        if (rnk[x] > rnk[y])
+            parent[y] = x;
+        else // If rnk[x] <= rnk[y]
+            parent[x] = y;
+
+        if (rnk[x] == rnk[y])
+            rnk[y]++;
+    }
+};
+
+ /* Functions returns weight of the MST*/
+
+int Graph::kruskalMST()
+{
+    int mst_wt = 0; // Initialize result
+
+    // Sort edges in increasing order on basis of cost
+    sort(edges.begin(), edges.end());
+
+    // Create disjoint sets
+    DisjointSets ds(V);
+
+    // Iterate through all sorted edges
+    vector< pair<int, iPair> >::iterator it;
+    for (it=edges.begin(); it!=edges.end(); it++)
+    {
+        int u = it->second.first;
+        int v = it->second.second;
+
+        int set_u = ds.find(u);
+        int set_v = ds.find(v);
+
+        // Check if the selected edge is creating
+        // a cycle or not (Cycle is created if u
+        // and v belong to same set)
+        if (set_u != set_v)
+        {
+            // Current edge will be in the MST
+            // so print it
+            cout << u << " - " << v << endl;
+
+            // Update MST weight
+            mst_wt += it->first;
+
+            // Merge two sets
+            ds.merge(set_u, set_v);
+        }
+    }
+
+    return mst_wt;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 /*	KMP Algorithm for pattern matching	*/
 
