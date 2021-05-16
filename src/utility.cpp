5c4e360508eeca4ce2557a97ee9e2442454a26b7
Ashish Singh Negi <asnecemnnit@gmail.com>
2021-05-16 19:09:11 +0200

    Added Segment Tree in utility

--- src/utility.cpp
+++ src/utility.cpp
@@ -1558,3 +1558,53 @@ void updateBIT(int n, int index, int val)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+/*	Segment Tree
+	tree Construction : O( n )
+	Query in Range : O( Log n )
+	Updating an element : O( Log n ).	*/
+
+const int N = 100000;
+// Max size of tree
+int tree[2 * N];
+
+// function to build the tree
+void build(vector<int> arr, int n)
+{
+	// insert leaf nodes in tree
+	for (int i = 0; i < n; i++)
+		tree[n + i] = arr[i];
+
+	// build the tree by calculating parents
+	for (int i = n - 1; i > 0; --i)
+		tree[i] = tree[i << 1] + tree[i << 1 | 1];
+}
+
+// function to update a tree node
+void updateTreeNode(int p, int value, int n)
+{
+	// set value at position p
+	tree[p + n] = value;
+	p = p + n;
+
+	// move upward and update parents
+	for (int i = p; i > 1; i >>= 1)
+		tree[i >> 1] = tree[i] + tree[i ^ 1];
+}
+
+// function to get sum on interval [l, r)
+int query(int l, int r, int n)
+{
+	int res = 0;
+
+	// loop to find the sum in the range
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+	{
+		if (l & 1)
+			res += tree[l++];
+
+		if (r & 1)
+			res += tree[--r];
+	}
+
+	return res;
+}
