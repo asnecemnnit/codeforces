9cd4c759dd390b0f7a8c197f9be26f54c10450d8
Ashish Negi <asnecemnnit@gmail.com>
2021-05-28 05:15:29 +0200

    Added feature to open files using sublime text after generating them

--- scripts/generate_file.sh
+++ scripts/generate_file.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 python3 generate_file.py $1
+subl ../../codeforces
+subl ../ProblemSet/$1.cpp
--- scripts/generate_files.sh
+++ scripts/generate_files.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 python3 generate_files.py $1
+subl ../../codeforces
+subl -p ../CurrentContest/*.cpp
