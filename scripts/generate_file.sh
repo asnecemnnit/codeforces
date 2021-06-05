f5047f104dff5cebc0d03d03fa8245f49b10b61c
Ashish Negi <asnecemnnit@gmail.com>
2021-06-06 00:22:57 +0200

    updated generate_file/s scripts to open utility.cpp by default

--- scripts/generate_file.sh
+++ scripts/generate_file.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 python3 generate_file.py $1
 subl ../../codeforces
+subl ../src/utility.cpp
 subl ../ProblemSet/$1.cpp
--- scripts/generate_files.sh
+++ scripts/generate_files.sh
@@ -2,4 +2,5 @@
 mkdir ../CurrentContest
 python3 generate_files.py $1
 subl ../../codeforces
+subl ../src/utility.cpp
 subl -p ../CurrentContest/*.cpp
